19b036447359b1e145a481f214498c6454ca49d2
emtkmkk <emtk@mkkey.net>
2023-09-06 18:03:22 +0900

    src/modules/emoji-react/index.ts を更新

--- src/modules/emoji-react/index.ts
+++ src/modules/emoji-react/index.ts
@@ -70,14 +70,14 @@ export default class extends Module {
 		if (includes(note.text, ['寿司', 'sushi']) || note.text === 'すし') return react('🍣');*/
 		if (includes(note.text, ['ぴざ'])) return react(':itspizzatime:');
 		if (includes(note.text, ['かんぴろばくたー','campylobacter'])) return react(':campylobacter_mottenaidesu:');
-		if (includes(note.text, ['もこもこ'])) return react(':mokomoko:');
-		if (includes(note.text, ['もこ'])) return react(':mk_chicken_t:');
-		if (includes(note.text, ['むいみ','無意味','muimi'])) return react(':osiina:');
-		if (includes(note.text, ['うそ','嘘','uso'])) return react(':sonnano_uso:');
-		if (includes(note.text, ['めつ','滅','metu'])) return react(':metu:');
-		if (includes(note.text, ['おはよ', 'ohayo'])) return react(':mk_oha:');
+		if (includes(note.text, ['おはよ', 'ohayo', 'おきた', '起きた', 'おっは'])) return react(':mk_oha:');
 		if (includes(note.text, ['おやす', 'oyasu'])) return react(':oyasumi2:');
 		if (includes(note.text, ['taikin', '退勤', 'たいきん'])) return react(':otukaresama:');
+		if (includes(note.text, ['うそ','嘘','uso'])) return react(':sonnano_uso:');
+		if (includes(note.text, ['めつ','滅','metu'])) return react(':metu:');
 		if (includes(note.text, ['つら','辛','しんど','帰りたい','かえりたい'])) return react(':petthex:');
+		if (includes(note.text, ['むいみ','無意味','muimi'])) return react(':osiina:');
+		if (includes(note.text, ['もこもこ'])) return react(':mokomoko:');
+		if (includes(note.text, ['もこ'])) return react(':mk_chicken_t:');
 	}
 }
