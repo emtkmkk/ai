6a692e85e22c0ab2378a4ff6ab89cbaed7027f6a
emtkmkk <emtk@mkkey.net>
2023-09-08 16:48:05 +0900

    ようこそを送ったかどうか覚えておく

--- src/modules/welcome/index.ts
+++ src/modules/welcome/index.ts
@@ -1,5 +1,6 @@
 import autobind from 'autobind-decorator';
 import Module from '@/module';
+import Friend, { FriendDoc } from '@/friend';
 
 export default class extends Module {
 	public readonly name = 'welcome';
@@ -16,26 +17,31 @@ export default class extends Module {
 	@autobind
 	private onLocalNote(note: any) {
 		if (!note.user.isBot && note.isFirstNote) {
-			setTimeout(() => {
-				this.ai.api('notes/create', {
-					renoteId: note.id,
-					localOnly: true
-				});
-			}, 3000);
-			
-			setTimeout(() => {
-				this.ai.api('notes/reactions/create', {
-					noteId: note.id,
-					reaction: ':mk_hi:'
-				});
-			}, 5000);
+			const friend = new Friend(this, { user: note.user });
+			if (!friend.doc.isWelcomeMessageSent){
+				friend.doc.isWelcomeMessageSent = true;
+				friend.save();
+				setTimeout(() => {
+					this.ai.api('notes/create', {
+						renoteId: note.id,
+						localOnly: true
+					});
+				}, 3000);
+				
+				setTimeout(() => {
+					this.ai.api('notes/reactions/create', {
+						noteId: note.id,
+						reaction: ':mk_hi:'
+					});
+				}, 5000);
 
-			setTimeout(() => {
-				this.ai.api('notes/reactions/create', {
-					noteId: note.id,
-					reaction: ':youkoso:'
-				});
-			}, 7000);
+				setTimeout(() => {
+					this.ai.api('notes/reactions/create', {
+						noteId: note.id,
+						reaction: ':youkoso:'
+					});
+				}, 7000);
+			}
 		}
 	}
 }
