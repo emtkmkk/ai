cfa0a3158a2de4157d11e333af79cc6af0c754be
emtkmkk <emtk@mkkey.net>
2023-09-09 20:53:00 +0900

    src/modules/poll/index.ts を更新

--- src/modules/poll/index.ts
+++ src/modules/poll/index.ts
@@ -108,7 +108,7 @@ export default class extends Module {
 		let choices = [
 			genItem(),
 			genItem(),
-			...(exist.keyword ? [exist.keyword] : []),
+			...(exist?.keyword ? [exist.keyword] : []),
 			genItem(),
 			genItem(),
 		];
