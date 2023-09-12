7cf59dbba179d6a27cc914aea8776591287c597a
emtkmkk <emtk@mkkey.net>
2023-09-12 11:32:14 +0900

    src/modules/welcome/index.ts を更新

--- src/modules/welcome/index.ts
+++ src/modules/welcome/index.ts
@@ -1,6 +1,7 @@
 import autobind from 'autobind-decorator';
 import Module from '@/module';
 import Friend, { FriendDoc } from '@/friend';
+import { acct } from '@/utils/acct';
 
 export default class extends Module {
 	public readonly name = 'welcome';
@@ -23,6 +24,14 @@ export default class extends Module {
 				friend.save();
 				setTimeout(() => {
 					this.ai.api('notes/create', {
+						text: `${acct(note.user)}さん\n**もこきーへようこそ！**\nわたしはもこもこチキン、このサーバーのマスコットみたいな存在です！\nお暇な時に挨拶していただいたり、占いや迷路などで遊んでくだされば嬉しいです！\nこれからよろしくお願いします！\n\nこのサーバーには独自機能が沢山あるので、気になったらお知らせの中にあるもこきーTipsを読んでみるといいかもです！\nなにか分からないことや嫌なこと、不便なことがあれば、気軽にもこきーあどみんに声をかけてください！\nそれでは楽しいもこきーライフを～ :mk_hi:`,
+						replyId: note.id,
+						visibility: 'specified',
+						visibleUserIds: [userId],
+					});
+				}, 8000);
+				setTimeout(() => {
+					this.ai.api('notes/create', {
 						renoteId: note.id,
 						localOnly: true
 					});
@@ -33,14 +42,14 @@ export default class extends Module {
 						noteId: note.id,
 						reaction: ':mk_hi:'
 					});
-				}, 5000);
+				}, 4500);
 
 				setTimeout(() => {
 					this.ai.api('notes/reactions/create', {
 						noteId: note.id,
 						reaction: ':youkoso:'
 					});
-				}, 7000);
+				}, 6000);
 			}
 		}
 	}
