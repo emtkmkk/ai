99ec02dc4839086d622e6d31d7a6629440f72d4c
emtkmkk <emtk@mkkey.net>
2023-09-20 14:47:39 +0900

    src/utils/safe-for-interpolate.ts を更新

--- src/utils/safe-for-interpolate.ts
+++ src/utils/safe-for-interpolate.ts
@@ -11,6 +11,20 @@ const invalidChars = [
 	'　',
 ];
 
+const ngword = ["ちんちん", "ちんぽ", "ちんこ", "うんこ", "うんち", "おしっこ", "ぱいぱい","おなほ", "おっぱい", "ぱいおつ", "ぱいずり", "乳首", "ちくび", "射精", "しゃせい", "おなに", "精液", "せいえき", "まんこ", "ふたなり", "れいぷ", "せっくす", "せくーす", "ヴぁぎな", "しこっ", "性器", "処女", "受精", "自慰", "勃起"];
+
+
 export function safeForInterpolate(text: string): boolean {
 	return !invalidChars.some(c => text.includes(c));
+}
+
+export function checkNgWord(text: string): boolean {
+	const chackText = text.replaceAll(/\s/g, "")
+		.replaceAll(/[!-\/:-@[-`{-~]+/g, "")
+		.replace(/[ァ-ン]/g, function (match) {
+			var chr = match.charCodeAt(0) - 0x60;
+			return String.fromCharCode(chr);
+		})
+		.replaceAll("ぱちんこ", "ぱチんこ")
+	return !ngword.some((x) => {chackText.includes(x);});
 }
