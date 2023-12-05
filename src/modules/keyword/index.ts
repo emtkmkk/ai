8ae781e05dc07b7976827e34878d68e38994e1ac
emtkmkk <emtk@mkkey.net>
2023-12-05 17:58:59 +0900

    Update index.ts

--- src/modules/keyword/index.ts
+++ src/modules/keyword/index.ts
@@ -1,6 +1,7 @@
 import autobind from 'autobind-decorator';
 import * as loki from 'lokijs';
 import Module from '@/module';
+import Message from '@/message';
 import config from '@/config';
 import serifs from '@/serifs';
 import { mecab } from './mecab';
