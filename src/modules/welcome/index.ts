04944dde9cfbfdf0b0d9bd28d8504e0443cad66c
emtkmkk <emtkmkk@gmail.com>
2023-12-28 09:39:47 +0900

    好感度がある程度あればノート数が一定の値を超えると通知するように

--- src/modules/welcome/index.ts
+++ src/modules/welcome/index.ts
@@ -20,37 +20,56 @@ export default class extends Module {
 	private onLocalNote(note: any) {
 		if (!note.user?.isBot && !note.user.host) {
 			const friend = new Friend(this.ai, { user: note.user });
-			if (!friend?.doc?.isWelcomeMessageSent && (note.isFirstNote || (friend.doc.user.notesCount && friend.doc.user.notesCount < 50)) ){
+			const data = friend.getPerModulesData(this)
+			if (!data.nextNotificationNotesCount) {
+				data.nextNotificationNotesCount = this.getNextNotification(note.user.notesCount);
+				friend.setPerModulesData(this, data);
+			}
+			// ノート数キリ番
+			else if ((friend.love || 0) >= 20 && ["public", "home"].includes(note.visibility) && !note.cw && note.user.notesCount >= data.nextNotificationNotesCount) {
+				data.nextNotificationNotesCount = this.getNextNotification(note.user.notesCount);
+				friend.setPerModulesData(this, data);
+				setTimeout(() => {
+					this.ai.api('notes/create', {
+						text: serifs.welcome.kiriban(data.nextNotificationNotesCount, acct(note.user)),
+						replyId: note.id,
+						visibility: 'specified',
+						visibleUserIds: [note.user.id],
+					});
+				}, 3000);
+			}
+			// 最初の投稿
+			if (!friend?.doc?.isWelcomeMessageSent && (note.isFirstNote || (note.user.notesCount && note.user.notesCount < 50))) {
 				friend.doc.isWelcomeMessageSent = true;
 				friend.save();
 				if (note.isFirstNote) {
-				setTimeout(() => {
-					this.ai.api('notes/create', {
-						renoteId: note.id,
-						localOnly: true
-					});
-				}, 3000);
+					setTimeout(() => {
+						this.ai.api('notes/create', {
+							renoteId: note.id,
+							localOnly: true
+						});
+					}, 3000);
 
-				setTimeout(() => {
-					this.ai.api('notes/reactions/create', {
-						noteId: note.id,
-						reaction: ':mk_hi:'
-					});
-				}, 4500);
+					setTimeout(() => {
+						this.ai.api('notes/reactions/create', {
+							noteId: note.id,
+							reaction: ':mk_hi:'
+						});
+					}, 4500);
 
-				setTimeout(() => {
-					this.ai.api('notes/reactions/create', {
-						noteId: note.id,
-						reaction: ':youkoso:'
-					});
-				}, 5500);
+					setTimeout(() => {
+						this.ai.api('notes/reactions/create', {
+							noteId: note.id,
+							reaction: ':youkoso:'
+						});
+					}, 5500);
 
-				setTimeout(() => {
-					this.ai.api('notes/reactions/create', {
-						noteId: note.id,
-						reaction: ':supertada:'
-					});
-				}, 6500);
+					setTimeout(() => {
+						this.ai.api('notes/reactions/create', {
+							noteId: note.id,
+							reaction: ':supertada:'
+						});
+					}, 6500);
 
 				}
 
@@ -63,6 +82,17 @@ export default class extends Module {
 					});
 				}, 8000);
 			}
+		}
+	}
+
+	@autobind
+	private getNextNotification(notesCount: number) {
+		const target = [50, 100, 500, 1000, 5000]
+		const clearTarget = target.filter((x) => x <= notesCount)
+		if (target.length === clearTarget.length) {
+			return (Math.floor(notesCount / 5000) + 1) * 5000
+		} else {
+			return target[clearTarget.length];
 		}
 	}
 }
