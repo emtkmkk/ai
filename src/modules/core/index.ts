24e072195fa7a956bd3fa13181ead08ef22ef3b5
emtkmkk <emtk@mkkey.net>
2024-04-14 12:39:42 +0900

    Update index.ts

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -50,7 +50,7 @@ export default class extends Module {
 			this.version(msg)
 		);
 
-		return ret.reaction ? ret : !!ret ? { reaction: "love" } : false;
+		return ret.reaction & !ret ? ret : { reaction: "love" };
 	}
 
 	@autobind
