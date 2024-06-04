d7454fd9a4957ded4f4fc21693011332b225de3e
emtkmkk <emtkmkk@gmail.com>
2024-06-04 12:50:28 +0900

    fix

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -70,15 +70,15 @@ export default class extends Module {
 		if (doc == null) return { reaction: ":mk_hotchicken:" };
 
 		let json = Array.isArray(doc) ? [ ...doc ] : { ...doc }
-
+		console.log("json : " + JSON.stringify(json, null, 2).length)
 		try {
 			if (Array.isArray(json)) {
 				for (let i = 0; i < json.length; i++) {
 					for (let key2 in json[i].user) {
-						if (json[i].user[key2] !== null && Array.isArray(json[i].user[key2])) {
+						if (json[i].user[key2] != null && Array.isArray(json[i].user[key2])) {
 							console.log("json[" + i + "].user[" + key2 + "] is Array")
 							json[i].user[key2] = "[Array]";
-						} else if (typeof json[i].user[key2] === 'object' && json[i].user[key2] !== null) {
+						} else if (typeof json[i].user[key2] === 'object' && json[i].user[key2] != null) {
 							console.log("json[" + i + "].user[" + key2 + "] is Object")
 							json[i].user[key2] = json[i].user[key2].name || "[Object]";
 						}
@@ -87,10 +87,10 @@ export default class extends Module {
 			} else {
 				for (let key in json) {
 					for (let key2 in json[key].user) {
-						if (Array.isArray(json[key].user[key2])) {
+						if (json[key].user[key2] != null && Array.isArray(json[key].user[key2])) {
 							console.log("json[" + key + "].user[" + key2 + "] is Array")
 							json[key].user[key2] = "[Array]";
-						} else if (typeof json[key].user[key2] === 'object' && json[key].user[key2] !== null) {
+						} else if (typeof json[key].user[key2] === 'object' && json[key].user[key2] != null) {
 							console.log("json[" + key + "].user[" + key2 + "] is Object")
 							json[key].user[key2] = json[key].user[key2].name || "[Object]";
 						}
