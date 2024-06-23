a22e9e0e195c8d27fb8ec18f495e62729d326ccd
emtkmkk <emtkmkk@gmail.com>
2024-06-23 21:51:05 +0900

    fix

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -81,7 +81,7 @@ export default class extends Module {
 
 		msg.friend.save();
 
-		if (filteredDoc[0].linkedAccounts.includes(msg.friend.userId)) {
+		if (filteredDoc[0].linkedAccounts?.includes(msg.friend.userId)) {
 			msg.reply(`アカウントのリンクに成功しました！\n投稿数が使用される際にリンクしたアカウントの合計投稿数で計算されるようになります！`);
 		} else {
 			msg.reply(`アカウントを登録しました！\nリンク先のアカウントからも同じ操作を実行してください！`);
