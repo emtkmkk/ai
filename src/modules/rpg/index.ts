ce97f9eb8e558b5e515106e5adec481f76898c24
emtkmkk <emtkmkk@gmail.com>
2024-06-23 22:00:04 +0900

    fix

--- src/modules/rpg/index.ts
+++ src/modules/rpg/index.ts
@@ -297,6 +297,25 @@ export default class extends Module {
                 postCount = postCount + (Math.min(Math.max(10, postCount / 2), 25) * continuousBonus)
             }
 
+            if (msg.friend.doc.linkedAccounts?.length) {
+                for (const userId of msg.friend.doc.linkedAccounts) {
+                    const friend = this.ai.lookupFriend(userId);
+                    if (!friend || !friend.doc?.linkedAccounts?.includes(msg.friend.userId)) continue;
+
+                    // ユーザの投稿数を取得
+                    const chart = await this.ai.api('charts/user/notes', {
+                        span: 'day',
+                        limit: 2,
+                        userId: userId
+                    })
+
+                    postCount += Math.max(
+                        (chart.diffs.normal?.[0] ?? 0) + (chart.diffs.reply?.[0] ?? 0) + (chart.diffs.renote?.[0] ?? 0) + (chart.diffs.withFile?.[0] ?? 0),
+                        (chart.diffs.normal?.[1] ?? 0) + (chart.diffs.reply?.[1] ?? 0) + (chart.diffs.renote?.[1] ?? 0) + (chart.diffs.withFile?.[1] ?? 0)
+                    );
+                }
+            }
+
             // 投稿数に応じてステータス倍率を得る
             // 連続プレイの場合は倍率アップ
             let tp =
