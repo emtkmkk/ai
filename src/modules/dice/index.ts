61479573df0bad46ddd6146b97065315b43248bd
Yasaka <007viz143@gmail.com>
2024-06-25 23:29:38 +0900

    Add to answer with yes or no

--- src/modules/dice/index.ts
+++ src/modules/dice/index.ts
@@ -4,49 +4,74 @@ import Message from "@/message";
 import serifs from "@/serifs";
 
 export default class extends Module {
-  public readonly name = "dice";
+	public readonly name = "dice";
 
-  @autobind
-  public install() {
-    return {
-      mentionHook: this.mentionHook,
-    };
-  }
+	@autobind
+	public install() {
+		return {
+			mentionHook: this.mentionHook,
+		};
+	}
 
-  @autobind
-  private async mentionHook(msg: Message) {
-    if (msg.text == null) return false;
+	@autobind
+	private async mentionHook(msg: Message) {
+		if (msg.text == null) return false;
 
-    const query = msg.text.match(/([0-9]+)[dD]([0-9]+)/);
+		if (
+			msg.text.endsWith("ですか？") ||
+			msg.text.endsWith("ますか？") ||
+			msg.text.endsWith("ですよね？")
+		) {
+			if (Math.random() < 0.5) {
+				if (Math.random() < 0.25) {
+					msg.reply("多分そうじゃ、部分的にそうじゃ", { visibility: "public" });
+				} else {
+					msg.reply("そうじゃ", { visibility: "public" });
+				}
+			} else {
+				if (Math.random() < 0.25) {
+					msg.reply("多分ちがうのじゃ、部分的にちがうのじゃ", {
+						visibility: "public",
+					});
+				} else {
+					msg.reply("ちがうのじゃ", { visibility: "public" });
+				}
+			}
+			return {
+				reaction: ":neofox_heart:",
+			};
+		}
 
-    if (query == null) return false;
+		const query = msg.text.match(/([0-9]+)[dD]([0-9]+)/);
 
-    const times = parseInt(query[1], 10);
-    const dice = parseInt(query[2], 10);
+		if (query == null) return false;
 
-    if (times < 1) return false;
-    if (dice < 2 || dice > Number.MAX_SAFE_INTEGER) return false;
+		const times = parseInt(query[1], 10);
+		const dice = parseInt(query[2], 10);
 
-    if ((dice.toString().length + 1) * times > 7000) return false;
+		if (times < 1) return false;
+		if (dice < 2 || dice > Number.MAX_SAFE_INTEGER) return false;
 
-    const results: number[] = [];
+		if ((dice.toString().length + 1) * times > 7000) return false;
 
-    for (let i = 0; i < times; i++) {
-      results.push(Math.floor(Math.random() * dice) + 1);
-    }
+		const results: number[] = [];
 
-    msg.reply(
-      serifs.dice.done(
-        results.join(" "),
-        results.length > 1
-          ? results.reduce((a, c) => a + c).toLocaleString()
-          : null
-      ),
-      { visibility: "public" }
-    );
+		for (let i = 0; i < times; i++) {
+			results.push(Math.floor(Math.random() * dice) + 1);
+		}
 
-    return {
-      reaction: "love",
-    };
-  }
+		msg.reply(
+			serifs.dice.done(
+				results.join(" "),
+				results.length > 1
+					? results.reduce((a, c) => a + c).toLocaleString()
+					: null
+			),
+			{ visibility: "public" }
+		);
+
+		return {
+			reaction: ":neofox_heart:",
+		};
+	}
 }
