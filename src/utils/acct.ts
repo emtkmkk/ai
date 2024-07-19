729e805bc36c33c36b0b4f70059172a15a14512d
emtkmkk <emtkmkk@gmail.com>
2024-07-19 22:11:50 +0900

    Update acct.ts

--- src/utils/acct.ts
+++ src/utils/acct.ts
@@ -1,5 +1,6 @@
-export function acct(user: { username: string; host?: string | null; }): string {
+export function acct(user: { username: string; host?: string | null; }, includeMyHost = false): string {
+	const myHost = "mkkey.net"
 	return user.host
 		? `@${user.username}@${user.host}`
-		: `@${user.username}`;
+		: includeMyHost ? `@${user.username}@${myHost}` : `@${user.username}`;
 }
