2a37d09e69037054695c7f0ff3aba85728cfc94d
emtkmkk <emtkmkk@gmail.com>
2024-07-25 14:36:20 +0900

    メンションがどこにあっても認識されるようになったが、正しくメンションする必要かある

--- src/ai.ts
+++ src/ai.ts
@@ -155,7 +155,7 @@ export default class 藍 {
 		// メンションされたとき
 		mainStream.on('mention', async data => {
 			if (data.userId == this.account.id) return; // 自分は弾く
-			if (data.text && data.text.startsWith('@' + this.account.username)) {
+			if (data.text && data.text.toLowerCase().includes(data.user.host ? `@${this.account.username}@${config.host}` : '@' + this.account.username)) {
 				// Misskeyのバグで投稿が非公開扱いになる
 				if (data.text == null) data = await this.api('notes/show', { noteId: data.id });
 				this.onReceiveMessage(new Message(this, data));
@@ -165,7 +165,7 @@ export default class 藍 {
 		// 返信されたとき
 		mainStream.on('reply', async data => {
 			if (data.userId == this.account.id) return; // 自分は弾く
-			if (data.text && data.text.startsWith('@' + this.account.username)) return;
+			if (data.text && data.text.toLowerCase().includes(data.user.host ? `@${this.account.username}@${config.host}` : '@' + this.account.username)) return;
 			// Misskeyのバグで投稿が非公開扱いになる
 			if (data.text == null) data = await this.api('notes/show', { noteId: data.id });
 			this.onReceiveMessage(new Message(this, data));
