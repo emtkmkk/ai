41282d2b93adefa2c19aee3d7bf591ddefddc025
emtkmkk <emtkmkk@gmail.com>
2024-07-25 15:27:04 +0900

    Update acct.ts

--- src/utils/acct.ts
+++ src/utils/acct.ts
@@ -1,6 +1,6 @@
 import config from '@/config';
 export function acct(user: { username: string; host?: string | null; }, includeMyHost = false): string {
-	const myHost = new URL(config.host).host.replace(/\./g, '\\.');
+	const myHost = new URL(config.host).host;
 	return user.host
 		? `@${user.username}@${user.host}`
 		: includeMyHost ? `@${user.username}@${myHost}` : `@${user.username}`;
