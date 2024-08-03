3cd77c4ec221b25b399df9c2d9a8c990fec2a7b1
emtkmkk <emtkmkk@gmail.com>
2024-08-03 11:27:12 +0900

    Update shop.ts

--- src/modules/rpg/shop.ts
+++ src/modules/rpg/shop.ts
@@ -269,6 +269,7 @@ export const shopReply = async (module: rpg, ai: 藍, msg: Message) => {
     const data = initializeData(module, msg);
     if (!data) return false;
     if (!data.lv) return false;
+	if (data.jar == null) data.jar = 0;
 
     // 所持しているスキル効果を読み込み
     const skillEffects = aggregateSkillsEffects(data);
