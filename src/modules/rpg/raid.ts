ee9c83cabc64c431f626fde850cb92fa831142e6
emtkmkk <emtkmkk@gmail.com>
2024-08-22 17:25:29 +0900

    Update raid.ts

--- src/modules/rpg/raid.ts
+++ src/modules/rpg/raid.ts
@@ -252,8 +252,8 @@ function finish(raid: Raid) {
 	for (let attacker of sortAttackers) {
 		results.push(`${attacker.me} ${acct(attacker.user)}:\n${attacker.mark === ":blank:" && attacker.dmg === 100 ? "💯" : attacker.mark} Lv${String(attacker.lv).padStart(levelSpace, ' ')} ${attacker.count}ターン ${attacker.dmg.toLocaleString()}ダメージ`);
 		if (results.length <= 9) results.push(`:blank:<small>${[
-			attacker.skillsStr.skills,
-			attacker.skillsStr.amulet ? `お守り ${skillsStr.amulet}` : undefined
+			attacker.skillsStr?.skills,
+			attacker.skillsStr?.amulet ? `お守り ${skillsStr.amulet}` : undefined
 		].filter(Boolean).join(" ")}</small>`);
 		if (references.length < 100) {
 			if (attacker.replyId) references.push(attacker.replyId);
