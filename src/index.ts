50c0ae4e766b06bdf3cf6128365f470701bf02c8
Yasaka <007viz143@gmail.com>
2024-08-26 22:14:56 +0900

    wikipedia機能/ngワード対応

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,100 @@
 // AiOS bootstrapper
 
-import "module-alias/register";
+import 'module-alias/register';
 
-import request from "request-promise-native";
-const promiseRetry = require("promise-retry");
+import request from 'request-promise-native';
+const promiseRetry = require('promise-retry');
 
-import 藍 from "./ai";
-import config from "./config";
-import _log from "./utils/log";
-const pkg = require("../package.json");
+import 藍 from './ai';
+import config from './config';
+import _log from './utils/log';
+const pkg = require('../package.json');
 
-import CoreModule from "./modules/core";
-import TalkModule from "./modules/talk";
-import BirthdayModule from "./modules/birthday";
-import PingModule from "./modules/ping";
-import EmojiModule from "./modules/emoji";
-import EmojiReactModule from "./modules/emoji-react";
-import FortuneModule from "./modules/fortune";
-import GuessingGameModule from "./modules/guessing-game";
-import KazutoriModule from "./modules/kazutori";
-import KeywordModule from "./modules/keyword";
-import WelcomeModule from "./modules/welcome";
-import TimerModule from "./modules/timer";
-import DiceModule from "./modules/dice";
-import ServerModule from "./modules/server";
-import FollowModule from "./modules/follow";
-import ValentineModule from "./modules/valentine";
-import MazeModule from "./modules/maze";
-import SleepReportModule from "./modules/sleep-report";
-import NotingModule from "./modules/noting";
-import PollModule from "./modules/poll";
-import ReminderModule from "./modules/reminder";
-import RpgModule from "./modules/rpg";
+import CoreModule from './modules/core';
+import TalkModule from './modules/talk';
+import BirthdayModule from './modules/birthday';
+import PingModule from './modules/ping';
+import EmojiModule from './modules/emoji';
+import EmojiReactModule from './modules/emoji-react';
+import FortuneModule from './modules/fortune';
+import GuessingGameModule from './modules/guessing-game';
+import KazutoriModule from './modules/kazutori';
+import KeywordModule from './modules/keyword';
+import WelcomeModule from './modules/welcome';
+import TimerModule from './modules/timer';
+import DiceModule from './modules/dice';
+import ServerModule from './modules/server';
+import FollowModule from './modules/follow';
+import ValentineModule from './modules/valentine';
+import MazeModule from './modules/maze';
+import SleepReportModule from './modules/sleep-report';
+import NotingModule from './modules/noting';
+import PollModule from './modules/poll';
+import ReminderModule from './modules/reminder';
+import RpgModule from './modules/rpg';
+import WikipediaModule from './modules/wikipedia';
 
-console.log("   __    ____  _____  ___ ");
-console.log("  /__\\  (_  _)(  _  )/ __)");
-console.log(" /(__)\\  _)(_  )(_)( \\__ \\");
-console.log("(__)(__)(____)(_____)(___/\n");
+console.log('   __    ____  _____  ___ ');
+console.log('  /__\\  (_  _)(  _  )/ __)');
+console.log(' /(__)\\  _)(_  )(_)( \\__ \\');
+console.log('(__)(__)(____)(_____)(___/\n');
 
 function log(msg: string): void {
-	_log(`[Boot]: ${msg}`);
+  _log(`[Boot]: ${msg}`);
 }
 
 log(`Ai v${pkg._v}`);
 
 promiseRetry(
-	(retry) => {
-		log(`Account fetching... ${config.host}`);
+  (retry) => {
+    log(`Account fetching... ${config.host}`);
 
-		// アカウントをフェッチ
-		return request
-			.post(`${config.apiUrl}/i`, {
-				json: {
-					i: config.i,
-				},
-			})
-			.catch(retry);
-	},
-	{
-		retries: 3,
-	}
+    // アカウントをフェッチ
+    return request
+      .post(`${config.apiUrl}/i`, {
+        json: {
+          i: config.i,
+        },
+      })
+      .catch(retry);
+  },
+  {
+    retries: 3,
+  },
 )
-	.then((account) => {
-		const acct = `@${account.username}`;
-		log(`Account fetched successfully: ${acct}`);
+  .then((account) => {
+    const acct = `@${account.username}`;
+    log(`Account fetched successfully: ${acct}`);
 
-		log("Starting AiOS...");
+    log('Starting AiOS...');
 
-		// 藍起動
-		new 藍(account, [
-			new CoreModule(),
-			new EmojiModule(),
-			new EmojiReactModule(),
-			new FortuneModule(),
-			new GuessingGameModule(),
-			new KazutoriModule(),
-			new TimerModule(),
-			new DiceModule(),
-			new TalkModule(),
-			new PingModule(),
-			new WelcomeModule(),
-			new ServerModule(),
-			new FollowModule(),
-			new BirthdayModule(),
-			//new ValentineModule(),
-			new KeywordModule(),
-			new MazeModule(),
-			new SleepReportModule(),
-			new NotingModule(),
-			new PollModule(),
-			new ReminderModule(),
-			new RpgModule(),
-		]);
-	})
-	.catch((e) => {
-		log("Failed to fetch the account");
-	});
+    // 藍起動
+    new 藍(account, [
+      new CoreModule(),
+      new EmojiModule(),
+      new EmojiReactModule(),
+      new FortuneModule(),
+      new GuessingGameModule(),
+      new KazutoriModule(),
+      new TimerModule(),
+      new DiceModule(),
+      new TalkModule(),
+      new PingModule(),
+      new WelcomeModule(),
+      new ServerModule(),
+      new FollowModule(),
+      new BirthdayModule(),
+      //new ValentineModule(),
+      new KeywordModule(),
+      new MazeModule(),
+      new SleepReportModule(),
+      new NotingModule(),
+      new PollModule(),
+      new ReminderModule(),
+      new RpgModule(),
+      new WikipediaModule(),
+    ]);
+  })
+  .catch((e) => {
+    log('Failed to fetch the account');
+  });
---
+++ src/modules/wikipedia/index.ts
@@ -0,0 +1,130 @@
+import autobind from 'autobind-decorator';
+import Module from '@/module';
+import { checkNgWord, ngword } from '@/utils/check-ng-word';
+import Message from '@/message';
+
+interface Article {
+  title: string;
+  url: string;
+}
+
+export default class extends Module {
+  public readonly name = 'wikipediaRandom';
+
+  @autobind
+  public install() {
+    this.post();
+    setInterval(this.post, 1000 * 60 * 3);
+
+    return {
+      mentionHook: this.mentionHook,
+    };
+  }
+
+  @autobind
+  private async mentionHook(msg: Message) {
+    if (msg.text) {
+      const keywords = ['wiki', 'ウィキ', 'うぃき'];
+      const manyKeywords = ['いっぱい', '沢山', 'たくさん'];
+
+      const containsKeyword = keywords.some((keyword) =>
+        msg.text.includes(keyword),
+      );
+      const containsManyKeyword = manyKeywords.some((keyword) =>
+        msg.text.includes(keyword),
+      );
+
+      if (containsKeyword) {
+        const articleCount = containsManyKeyword ? 3 : 1;
+        const articles = await this.fetchFilteredArticles(articleCount);
+
+        if (articles && articles.length > 0) {
+          const replyText = articles
+            .map(
+              (article) =>
+                `「${article.title}」の記事を読むのはどうじゃ？\n${article.url}`,
+            )
+            .join('\n\n');
+          msg.reply(replyText, {
+            immediate: true,
+          });
+        } else {
+          msg.reply(
+            '何だかうぃきぺでぃあとやらの調子が悪くてなにも見つからなかったのじゃ！わらわのせいじゃないのじゃ～！',
+            {
+              immediate: true,
+            },
+          );
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @autobind
+  private async post() {
+    const now = new Date();
+    if (now.getHours() !== 8) return;
+    const date = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
+    const data = this.getData();
+    if (data.lastPosted == date) return;
+    data.lastPosted = date;
+    this.setData(data);
+
+    const articles = await this.fetchFilteredArticles(1);
+    if (articles && articles.length > 0) {
+      const article = articles[0];
+      this.log('Posting random Wikipedia article...');
+
+      this.log('Posting...');
+      this.ai.post({
+        text: `今日は「${article.title}」の記事を読むのはどうじゃ？\n${article.url}`,
+      });
+    }
+  }
+  private async fetchFilteredArticles(
+    count: number,
+  ): Promise<Article[] | null> {
+    const URL = 'https://ja.wikipedia.org/w/api.php';
+    const RANDOM_PARAMS = new URLSearchParams({
+      action: 'query',
+      format: 'json',
+      list: 'random',
+      rnlimit: count.toString(),
+      rnnamespace: '0',
+    });
+
+    try {
+      const articles: Article[] = [];
+
+      while (articles.length < count) {
+        const randomResponse = await fetch(
+          `${URL}?${RANDOM_PARAMS.toString()}`,
+        );
+        const randomData = await randomResponse.json();
+
+        for (const articleData of randomData.query.random) {
+          const title = articleData.title;
+
+          // タイトルにNGワードが含まれているか確認
+          const isSafeTitle = checkNgWord(title);
+          if (isSafeTitle) {
+            articles.push({
+              title,
+              url: `https://ja.wikipedia.org/wiki/${encodeURIComponent(title)}`,
+            });
+
+            // 取得した記事が目標の数に達したらループを抜ける
+            if (articles.length >= count) break;
+          }
+        }
+      }
+
+      return articles.length > 0 ? articles : null;
+    } catch (error) {
+      console.error('記事の取得中にエラーが発生しました:', error);
+      return null;
+    }
+  }
+}
