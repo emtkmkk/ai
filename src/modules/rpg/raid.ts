965de0a445a6b2766e34e9569700e57c47a71b2a
emtkmkk <emtkmkk@gmail.com>
2024-10-02 12:34:52 +0900

    最大最後の一撃ダメージがある場合はそれを使用

--- src/modules/rpg/raid.ts
+++ src/modules/rpg/raid.ts
@@ -1313,7 +1313,8 @@ export async function getTotalDmg(msg, enemy: RaidEnemy) {
 		const enemySAtk = Math.max((_enemyAtk / (lv * 3.5)) * (getVal(enemy.atkx, [6]) ?? 3), 0.01);
 		let enemyFDef = (typeof enemy.def === "function") ? enemy.def(atk, def, spd) : lv * 3.5 * (enemy.def ?? 1);
 		const enemySDef = Math.max((enemyFDef / (lv * 3.5)) * (getVal(enemy.defx, [6]) ?? 3), enemySAtk / 3000);
-		let dmg = Math.round(playerHp / (playerMaxHp) * (1000 + (enemySAtk >= 24 ? enemySAtk / 0.048 : 0)) * Math.max(enemySAtk / enemySDef, 1) * (1 + (skillEffects.finalAttackUp ?? 0)));
+		let lastDmg = (1000 + (enemySAtk >= 24 ? enemySAtk / 0.048 : 0)) * Math.max(enemySAtk / enemySDef, 1);
+		let dmg = Math.round(playerHp / (playerMaxHp) * (enemy.maxLastDmg ? Math.min(lastDmg, enemy.maxLastDmg) : lastDmg) * (1 + (skillEffects.finalAttackUp ?? 0)));
 		if (sevenFever) {
 			dmg = dmg >= 777 ? Math.max(Math.floor((dmg - 777) / 1000) * 1000, 0) + 777 : dmg;
 		}
