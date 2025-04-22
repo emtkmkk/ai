508e03093f8457c728be8503fe99601c1e78e6eb
emtkmkk <emtkmkk@gmail.com>
2025-04-22 10:00:53 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -57,9 +57,9 @@ config.apiUrl = config.host + '/api';
 // 設定が存在しない場合はデフォルトを設定
 if (!config.instanceName) config.instanceName = "もこきー";
 if (!config.defaultVisibility) config.defaultVisibility = "public";
-if (!config.postNotPublic !== false) config.postNotPublic = true;
-if (!config.randomPostLocalOnly !== false) config.randomPostLocalOnly = true;
-if (!config.birthdayPostLocalOnly !== false) config.randomPostLocalOnly = true;
+if (config.postNotPublic !== false) config.postNotPublic = true;
+if (config.randomPostLocalOnly !== false) config.randomPostLocalOnly = true;
+if (config.birthdayPostLocalOnly !== false) config.randomPostLocalOnly = true;
 if (!config.rpgHeroName) config.rpgHeroName = "もこチキ";
 if (!config.rpgCoinName) config.rpgCoinName = "もこコイン";
 if (!config.rpgCoinShortName) config.rpgCoinShortName = "コイン";
