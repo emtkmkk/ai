b48132683c2ff4c3e99b6c201249729faa59cb66
emtkmkk <emtkmkk@gmail.com>
2025-09-25 13:27:23 +0900

    処理順誤り

--- src/modules/rpg/shop-custom.ts
+++ src/modules/rpg/shop-custom.ts
@@ -60,7 +60,7 @@ export const shopCustomReply = async (module: rpg, ai: 藍, msg: Message) => {
     const candidateStartIndex = currentSkills.length > 0 ? 2 : 1;
     const showCount = currentSkills.length > 0 ? 8 : 9;
 
-    const show = skills.sort(() => rnd() - 0.5).slice(0, showCount).filter((x) => !x.moveTo && !x.cantReroll && !x.unique && !x.skillOnly && !data.tempAmulet.includes(x.name));
+    const show = skills.sort(() => rnd() - 0.5).filter((x) => !x.moveTo && !x.cantReroll && !x.unique && !x.skillOnly && !data.tempAmulet.includes(x.name)).slice(0, showCount);
 
     let list = show.map((sk, i) => {
         const price = partPrice(ai, currentSkills, sk, data.tempAmuletCost);
